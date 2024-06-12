7bb0dea8a49358fcef621e432c1ff10c64011bcc
andrewdeanphillips <andrew-phillips98@hotmail.com>
2024-06-12 17:01:17 +1000

    added comments to recivedresponses

--- messagewall-frontend/src/components/RecievedResponses.jsx
+++ messagewall-frontend/src/components/RecievedResponses.jsx
@@ -1,4 +1,4 @@
-import Message from "./Message"
+import Message from "./Message";
 
 const RecievedResponses = ({
   questions,
@@ -9,21 +9,23 @@ const RecievedResponses = ({
   selectedFilter,
   handleVisibleQuestionsChange,
 }) => {
-
-
-  const renderMessages = (question) => {
-    return messages
-      .filter((message) => message.question === question)
-      .map((message) => (
-        <Message key={message.id} message={message} handleLike={handleLike} />
-      ));
+  // メッセージをレンダリングする関数
+  const renderMessage = (question) => {
+    return (
+      messages
+        // 特定の質問に関連するメッセージのみフィルタリングし、Messageコンポーネントを生成する
+        .filter((message) => message.question === question)
+        .map((message) => (
+          <Message key={message.id} message={message} handleLike={handleLike} />
+        ))
+    );
   };
-
 
   return (
     <div>
       <h1>いただいた答え</h1>
       <h3>フィルター</h3>
+      {/* 見える質問のメッセージをフィルター */}
       <select
         name="visibleQuestions"
         value={selectedFilter}
@@ -32,12 +34,14 @@ const RecievedResponses = ({
         <option key="all" value="all">
           全部
         </option>
+        {/* 全ての質問と、それぞれの質問に対する選択肢を生成 */}
         {questions.map((question) => (
           <option key={question} value={question}>
             {question}
           </option>
         ))}
       </select>
+      {/* visibleQuestionsをマップして表示 */}
       {visibleQuestions.map((q) => (
         <div key={q.question}>
           <div className="questionTitle">
@@ -45,7 +49,8 @@ const RecievedResponses = ({
               {q.question}
             </h2>
           </div>
-          {q.visible && <ul>{renderMessages(q.question)}</ul>}
+           {/* q.visible === true の場合はその質問に対応するメッセージを表示 */}
+          {q.visible && <ul>{renderMessage(q.question)}</ul>}
         </div>
       ))}
     </div>
